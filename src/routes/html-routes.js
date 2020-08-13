c213a2592b266c88a6c02372d700e84392447c98
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-08-13 12:45:04 +0100

    Added html routes to render all pages

--- src/routes/html-routes.js
+++ src/routes/html-routes.js
@@ -1,8 +1,23 @@
-const router = require("express").Router();
+const express = require("express");
 const path = require("path");
 
-router.get("/", (req, res) => {
-  res.render();
-});
+const router = express.Router();
+
+const statsPath = (req, res) => {
+  const filePath = path.join(__dirname, "../public/stats.html");
+  res.sendFile(filePath);
+};
+
+const exercisePath = (req, res) => {
+  const filePath = path.join(__dirname, "../public/exercise.html");
+  res.sendFile(filePath);
+};
+
+// router.get("/", (res, req) => {
+//   const filePath = path.join(__dirname, "../public");
+//   res.sendFile("/index.html", filePath);
+// });
+router.get("/exercise", exercisePath);
+router.get("/stats", statsPath);
 
 module.exports = router;
